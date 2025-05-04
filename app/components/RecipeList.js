3f08dbe519777e0403215af1a129ab3c03191b97
Jackeline Arellano <jackiea40818@gmail.com>
2025-05-03 18:53:55 -0700

    fix

--- app/components/RecipeList.js
+++ app/components/RecipeList.js
@@ -3,6 +3,7 @@
 import Image from "next/image";
 import Link from "next/link";
 import { useState, useEffect } from "react";
+import { ChevronDown } from "lucide-react";
 import { Heart } from "lucide-react";
 
 export default function RecipeList({ recipes = [], availableIngredients = [] }) {
